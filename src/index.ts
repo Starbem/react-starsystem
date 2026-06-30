4e9050b23ef2531fcc29dfc8cfe70ddd4ad8509f
Julio Sousa <julioams@icloud.com>
2026-06-30 17:52:30 -0300

    feat(input): update barrel exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import './styles/globals.css'
 export { Button } from './components/Button'
 export type { ButtonProps } from './components/Button'
 
+export { Input } from './components/Input'
+export type { InputProps } from './components/Input'
+
 // Design tokens
 export { colors } from './tokens/colors'
 export type { Colors } from './tokens/colors'
